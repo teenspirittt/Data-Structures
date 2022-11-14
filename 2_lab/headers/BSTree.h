d3ba481187a4c54417e5561fd5ea7f8a94210e4d
badbadblues _ <borovikxv.a@gmail.com>
2022-11-14 14:22:46 +0700

    assigned tasks

--- 2_lab/headers/BSTree.h
+++ 2_lab/headers/BSTree.h
@@ -6,6 +6,7 @@ typedef unsigned int uint;
 template <typename T>
 struct Node {
     T value;
+    int key;
     Node* left;
     Node* right;
     Node(T value) {this->value = value;}
@@ -14,5 +15,20 @@ struct Node {
 template <typename T>
 class BSTree {
     public:
+    BSTree() {}
+    BSTree(const BSTree& copy);
+    ~BSTree(){}
+    uint size();
+    void clear();
+    boolean isEmpty();
+    T get(int key);
+    T set(int key);
+    T insert(T value, int key);
+    bool remove(int key);
+    void printKeys();
+    void printTree();
+    
     private:
+        uint size{};
+        Node<T>* root{};
 };
